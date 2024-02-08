6322da4c1a1d1f54fd48e01f7c41b81ced23667d
TylerChu1234 <tylerchu.tyc@gmail.com>
2024-02-08 11:49:23 -0500

    updated printingcomments.java

--- PrintingComments.java
+++ PrintingComments.java
@@ -7,15 +7,28 @@ class PrintingComments extends ConsoleProgram {
   
   public void run() {
 
-    // Say hello
-    
-
+    // say hello
+    System.out.println("hi");
+    System.out.println("there");
     // Say hello on the same line
-    
+    System.out.print("hi ");
+    System.out.print("there ");
     
     // multiline comment
     
-    
+    /*
+     * this is a a mlultiline comment
+     */
+
+
+
+
+
+
+
+
+
+
 
   }
 }
